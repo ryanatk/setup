a68a520b82f80b7b2f958019ed9540d7003a1032
Ryan Atkinson <ryana804@gmail.com>
2013-03-04 08:45:16 -0800

    updated package.json, added comments

--- install.js
+++ install.js
@@ -3,16 +3,22 @@ var path = require('path');
 var request = require('request');
 var argv = require('optimist').argv;
 
-var targets = path.join(__dirname, 'targets');
+/** setup must live in ~/.setup **/
+
+// calculate and cache dir paths, timestamp
 var home = path.join(__dirname, '..');
+var targets = path.join(__dirname, 'targets');
 var timestamp = new Date().getTime();
 
+// create prototype for each file
 var File = function (filename) {
   this.filename = filename;
   this.dotname = path.join(home, '.' + filename);
 };
 
+// extend file prototype to include functions
 File.prototype = {
+  // create a symlink from dotfile location to target code
   addSymlink: function (file) {
     var target = path.join(targets, file.filename);
     var dotname = file.dotname
@@ -22,6 +28,7 @@ File.prototype = {
     });
   },
 
+  // backup the file, adding timestamp
   backup: function (callback) {
     var self = this;
     var dotname = self.dotname;
@@ -29,12 +36,13 @@ File.prototype = {
 
     fs.move(dotname, bkup, function (err) {
       if (!err) console.log('Created backup:', bkup);
+      // execute callback
       if (callback) callback(self);
     });
   }
 };
 
-// get target files for symlinking
+// get all target files for symlinking
 (function () {
   fs.readdir(targets, function (err, filenames) {
     if (err) throw err;
@@ -49,14 +57,12 @@ File.prototype = {
 }());
 
 // git completion
+// fired by adding option --update
 (function (update) {
   if (update) {
+    // download from github to write to file
     request({'uri': 'https://raw.github.com/git/git/master/contrib/completion/git-completion.bash'}, function (err, res, body) {
-      if (err) {
-        throw err;
-        console.log('Failed to download git completion');
-      }
-      else if (!body) {
+      if (err || !body) {
         console.log('Failed to download git completion');
       } else {
         fs.writeFile('./git-completion.sh', body);
@@ -67,11 +73,12 @@ File.prototype = {
 }(argv.update || null));
 
 // bash profile
+// fired by adding option --profile=machine_name
 (function (profile) {
   if (profile) {
     var file = new File('bash_profile');
 
-    var data = [
+    var content = [
       'export profile_name=\'' + profile + '\'',
       'source .setup/bash_profiles/main.bash',
       '#source .setup/bash_profiles/home.bash',
@@ -80,10 +87,11 @@ File.prototype = {
       '#source .setup/bash_profiles/osx.bash'
     ];
 
+    // TODO: add this to prototype
     var writeFile = function (file) {
-      fs.writeFile(file.dotname, data.join('\n'), 'utf8', function () {
+      fs.writeFile(file.dotname, content.join('\n'), 'utf8', function () {
         console.log('wrote to:', file.dotname);
-        // need to reload bash_profile
+        // need to figure out how to reload bash_profile
       });
     };
     file.backup(writeFile);
