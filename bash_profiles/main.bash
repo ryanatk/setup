8d3d23473adbf75ee1227624a0fdc25c471f7e95
Ryan Atkinson <ryana804@gmail.com>
2013-06-29 16:21:08 -0700

    added grep exclude for node_modules/*

--- bash_profiles/main.bash
+++ bash_profiles/main.bash
@@ -15,7 +15,7 @@ alias ls="ls -G"
 alias ll='ls -alF'
 alias la='ls -A'
 alias lc='ls -CF'
-alias gr='grep -r'
+alias gr='grep -r --exclude=node_modules/*'
 alias fin='find . -name'
 alias myip='ipconfig getifaddr en0'
 alias myipwireless='ipconfig getifaddr en1'
