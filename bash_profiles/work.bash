07e51dec59f5823ee4d4ef4609454b866548b786
ryanatk <ryana804@gmail.com>
2014-12-11 23:37:20 -0500

    updated curla

--- bash_profiles/work.bash
+++ bash_profiles/work.bash
@@ -66,7 +66,7 @@ alias curlz='curl -H "X-ZFC-Debug: on" -I'
 alias curlsys='curl -qisv -o/dev/null'
 
 # get akamai headers back in curl (does not work in the office)
-alias curla='curl -H I -"Pragma: akamai-x-cache-on"'
+alias curla='curl -H I -"Pragma: akamai-x-cache-on" -I'
 
 # get all the akamai headers back in curl (does not work in the office)
 alias curlakamai='curl -qisv -H "Pragma: akamai-x-cache-on, akamai-x-cache-remote-on,  akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values,  akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key,  akamai-x-serial-no"'
